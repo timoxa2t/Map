e78b8e9fdff6742d737ccaef752861ea7c15753a
timoxa2t <alex2t172@gmail.com>
2023-05-31 19:25:07 +0300

    api connected

--- src/components/ModalForm/ModalForm.tsx
+++ src/components/ModalForm/ModalForm.tsx
@@ -1,35 +1,72 @@
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Spinner } from 'react-bootstrap';
 import styles from './ModalForm.module.scss';
 import { ChangeEvent, FormEvent, useState } from 'react';
+import { LatLng } from 'leaflet';
+import { Marker } from '../../types/Marker';
+import { createMarker } from '../../servises/api';
+
+type setMarkersCallback = (prev: Marker[]) => Marker[]
 
 interface Props {
-  callback?: (comment: string) => void,
+  setMarkers: (callback: setMarkersCallback) => void,
   setIsModalVisible: (visible: boolean) => void,
+  latLng: LatLng,
 }
 
 export const ModalForm: React.FC<Props> = ({
-  callback = (comment: string) => {},
+  setMarkers,
   setIsModalVisible,
+  latLng,
 }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [latitude, setLatitude] = useState(latLng.lat);
+  const [longitude, setLongitude] = useState(latLng.lng);
 
-  const [comment, setComment] = useState('');
+
+  const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
 
   const handleCommentChange = (event: ChangeEvent<HTMLInputElement>) => {
-    setComment(event.target.value);
+    setDescription(event.target.value);
+  }
+
+  const handleLatitudeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLatitude(Number(event.target.value));
+  }
+
+  const handleLongitudeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setLongitude(Number(event.target.value));
   }
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    if (!comment) {
+    if (!name) {
       return
     }
 
-    callback(comment);
-    setIsModalVisible(false);
+    setIsLoading(true);
+
+    createMarker({
+      name,
+      description,
+      latitude,
+      longitude,
+    }).then(newMarker => {
+      setMarkers(prevMarkers => [
+        ...prevMarkers,
+        newMarker,
+      ]);
+    })
+    .finally(() => {
+      setIsLoading(false);
+      setIsModalVisible(false);
+    })
   }
 
   const handleCancel = () => {
-    callback('');
     setIsModalVisible(false);
   }
 
@@ -37,24 +74,81 @@ export const ModalForm: React.FC<Props> = ({
     <div className={styles.container}>
       <div className={styles.modal}>
         <Form onSubmit={handleSubmit}>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Marker description</Form.Label>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
             <Form.Control
               type="text"
               placeholder="Home"
-              value={comment}
-              onChange={handleCommentChange}  
+              value={name}
+              onChange={handleNameChange}
+              required
+            />
+            <Form.Text className="text-muted">
+              Enter name for your marker
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Here I live"
+              value={description}
+              onChange={handleCommentChange}
             />
             <Form.Text className="text-muted">
               Enter description for your marker
             </Form.Text>
           </Form.Group>
 
-          <Button variant="success" type='submit'>Save</Button>{' '}
+          
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Latitude</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Home"
+              value={latitude}
+              onChange={handleLatitudeChange}
+              required
+            />
+          </Form.Group>
 
-          <Button variant="danger" onClick={handleCancel}>Cancel</Button>{' '}
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Longitude</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="Home"
+              value={longitude}
+              onChange={handleLongitudeChange}
+              required
+            />
+          </Form.Group>
+
+          <Button
+            variant="success"
+            type='submit'
+            disabled={isLoading}
+          >
+            Save
+          </Button>
+
+          {'  '}
+
+          <Button
+            variant="danger"
+            onClick={handleCancel}
+            disabled={isLoading}
+          >
+            Cancel
+          </Button>
         </Form>
       </div>
+      
+      {isLoading && (
+        <div className={styles.modal__spinner}>
+          <Spinner />
+        </div>
+      )}
     </div>
   );
 }
